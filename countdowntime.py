d378c20730aabb15014ca078bbb3a077144ed56b
Sukhbinder Singh <Sukhbinder@email.com>
2020-10-08 19:14:37 +0530

    Added announcing time option

--- countdowntime.py
+++ countdowntime.py
@@ -1,24 +1,65 @@
 from __future__ import print_function
 import time
 import argparse
+from multiprocessing import Process
+import os
 
 
+def announce(msg, sleepseconds=0.0):
+    process = Process(target=announce_inner,
+                      args=(msg, sleepseconds,), daemon=True)
+    process.start()
 
-def countdown(t):
+
+if os.name == "nt":
+    import win32com.client as wincl
+
+    def _say(sentence, sleepseconds=0.5):
+        try:
+            speaker = wincl.Dispatch("SAPI.SpVoice")
+            speaker.Speak(sentence)
+            time.sleep(sleepseconds)
+        except Exception as ex:
+            print("Error in speaking: ".format(ex.msg))
+else:
+    def _say(sentence, sleepseconds=0.5):
+        os.system("say {0}".format(sentence))
+        time.sleep(sleepseconds)
+
+
+def announce_inner(msg, sleepseconds):
+    _say(msg, sleepseconds)
+
+
+def countdown(t, sayit):
+    time2voice = []
+    if sayit:
+        time2voice = list(range(sayit*60, t, sayit*60))
+    # print(time2voice)
     while t:
         mins, secs = divmod(t, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
         print(timeformat, end='\r')
         time.sleep(1)
+        if t in time2voice:
+            msg = "{} mins remaining.".format(mins)
+            announce(msg)
         t -= 1
     print('Done!\n\a\a\a\a\a')
+    announce("Times up buddy!")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Countdown timer")
-    parser.add_argument("-s","--seconds" , type=int, help="Time in seconds", default=60)
+    parser.add_argument("-s", "--seconds", type=int,
+                        help="Time in seconds", default=60)
+    parser.add_argument("-v", "--say", type=int, default=0,
+                        help="Say the remaining every minute")
     args = parser.parse_args()
-    countdown(args.seconds)
 
+    process = Process(target=countdown,
+                      args=(args.seconds, args.say,), daemon=False)
+    process.start()
 
 
 if __name__ == "__main__":
